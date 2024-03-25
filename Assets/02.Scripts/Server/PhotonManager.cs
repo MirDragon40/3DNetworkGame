8425cb9964fbb0a4de0588213a1b0ed93c9051a7
MirDragon40 <mirjeong6463@gmail.com>
2024-03-25 12:36:08 +0900

    포톤 서버접속 완료

--- Assets/02.Scripts/Server/PhotonManager.cs
+++ Assets/02.Scripts/Server/PhotonManager.cs
@@ -15,6 +15,8 @@ public class PhotonManager : MonoBehaviourPunCallbacks   // PUN의 사양한 서
         // 순서:
         // 1. 게임 버전을 설정 한다.
         PhotonNetwork.GameVersion = "0.0.1";
+        // <전체를 뒤엎을 변화>, <기능 수정, 추가>, <버그, 내부적 코드 수정>
+
         // 2. 닉네임을 설정한다. 
         PhotonNetwork.NickName = $"정수빈_{UnityEngine.Random.Range(0, 100)}";
         // 3. 씬을 설정한다.
@@ -27,12 +29,87 @@ public class PhotonManager : MonoBehaviourPunCallbacks   // PUN의 사양한 서
     public override void OnConnected()
     {
         Debug.Log("서버 접속 성공");
+        Debug.Log(PhotonNetwork.CloudRegion);
     }
 
     public override void OnDisconnected(DisconnectCause cause)
     {
         Debug.Log("서버 연결 해제");
     }
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("마스터 서버 접속 성공");
+        Debug.Log($"InLobby?: {PhotonNetwork.InLobby}");
 
+        // 기본 호텔의 로비에 들어가겠다.
+        // 로비: 매치메이킹: (방 목록, 방 생성, 방 입장)
+        // - 로비 이름
+        // -  룸 목록, 룸 개수
+        // - 플레이어 수
+        PhotonNetwork.JoinLobby(TypedLobby.Default);
+
+    }
+
+
+    // override: 기본에 있던 메소드를 확장했다는 의미.
+    // 로비에 접속한 후 호출되는 콜백 함수
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("로비에 입장했습니다.");
+        Debug.Log($"InLobby?: {PhotonNetwork.InLobby}");
+
+
+        //PhotonNetwork.CreateRoom();            // 방을 만드는 것
+        //PhotonNetwork.JoinRoom();              // 방에 입장하는 것
+        //PhotonNetwork.JoinRandomRoom();        // 랜덤한 방에 입장하는 것
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 20;  // 입장 가능한 최대 플레이어 수
+        roomOptions.IsVisible = true; // 로비에서 방 목록에 노출할 것인가?
+        PhotonNetwork.JoinOrCreateRoom("Test", roomOptions, TypedLobby.Default);      // 방이 있다면 입장하고 없다면 만드는 것
+        //PhotonNetwork.JoinRandomOrCreateRoom();  // 랜덤한 방에 들어가거나 없다면 만드는 것
+    }
+
+    // 방 생성에 성공했을 때 호출되는 콜백 함수
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("방 생성 성공!");
+        Debug.Log($"RoomName: {PhotonNetwork.CurrentRoom.Name}");
+    }
+
+
+    // 방에 들어갔을 때 호출되는 함수
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("방 입장 성공");
+        Debug.Log($"RoomName: {PhotonNetwork.CurrentRoom.Name}");
+        Debug.Log($"RoomName: {PhotonNetwork.CurrentRoom.PlayerCount}");
+        Debug.Log($"RoomName: {PhotonNetwork.CurrentRoom.MaxPlayers}");
+
+    }
+
+
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("OnJoinRoomFailed");
+        Debug.Log(message);
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("OnJoinRandomFailed!");
+        Debug.Log(message);
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("OnCreateRoomFailed");
+        Debug.Log(message);
+    }
+
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        
+    }
 }
 
